4bd7bceb9682b622db2ac72dbb7f58013acb100c
rsmith6321 <rsmith@jinnawats-MacBook-Pro.local>
2020-04-29 17:12:29 +0700

    fix out of range error
    fix out of range condition by using try and catch. also as add new .jar file

--- StringToInt/src/main.java
+++ StringToInt/src/main.java
@@ -8,26 +8,30 @@ public class main {
 		String input = sc.nextLine();
 		String getDigit="";
 		
-		for(int i=0;i<input.length();i++) {
-			if(Character.isDigit(input.charAt(i))){
-				getDigit=getDigit+input.charAt(i);
-				
+		
+		try {
+			
+			for(int i=0;i<input.length();i++) {
+				if(Character.isDigit(input.charAt(i))){
+					getDigit=getDigit+input.charAt(i);
+					
+				}
+				else continue;
 			}
-			else continue;
+			
+			if(getDigit!="") {
+				int output=Integer.parseInt(getDigit);
+				System.out.println("Your output: "+output);
+			}
+			
+			else {
+				System.out.println("Your input has no digit.");
+			}
 		}
 		
 		
-		if(Long.parseLong(getDigit)>2147483647) {
-			System.out.println("Your input has too much digit.");
-		}
-		
-		else if(getDigit!="") {
-			int output=Integer.parseInt(getDigit);
-			System.out.println("Your output: "+output);
-		}
-		
-		else {
-			System.out.println("Your input has no digit.");
+		catch(Exception e){
+			System.out.println("Your input has out of Int's range.");
 		}
 		
 
