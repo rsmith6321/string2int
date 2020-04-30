3268eb81967d136e8e08b91cd8d3674b56e15e26
rsmith6321 <rsmith@jinnawats-MacBook-Pro.local>
2020-04-30 12:45:56 +0700

    change algorithm
    change algorithm to not use parsing function.

--- StringToInt/src/main.java
+++ StringToInt/src/main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 public class main {
 
 	public static void main(String[] args) {
@@ -8,33 +9,61 @@ public class main {
 		String input = sc.nextLine();
 		String getDigit="";
 		
-		
-		try {
 			
-			for(int i=0;i<input.length();i++) {
-				if(Character.isDigit(input.charAt(i))){
-					getDigit=getDigit+input.charAt(i);
+		for(int i=0;i<input.length();i++) {
+			if(Character.isDigit(input.charAt(i))){
+				getDigit=getDigit+input.charAt(i);
 					
-				}
-				else continue;
 			}
+			else continue;
+		}
 			
-			if(getDigit!="") {
-				int output=Integer.parseInt(getDigit);
+		if(getDigit!="") {
+
+			int output=0;
+			double exponent = 0;
+				
+			for(int i = getDigit.length()-1;i>=0;i--){
+					
+				switch(getDigit.charAt(i)) {
+				case '1' : output+=(1*Math.pow(10,exponent));
+				break;
+				case '2' : output+=(2*Math.pow(10,exponent));
+				break;
+				case '3' : output+=(3*Math.pow(10,exponent));
+				break;
+				case '4' : output+=(4*Math.pow(10,exponent));
+				break;
+				case '5' : output+=(5*Math.pow(10,exponent));
+				break;
+				case '6' : output+=(6*Math.pow(10,exponent));
+				break;
+				case '7' : output+=(7*Math.pow(10,exponent));
+				break;
+				case '8' : output+=(8*Math.pow(10,exponent));
+				break;
+				case '9' : output+=(9*Math.pow(10,exponent));
+				break;
+				case '0' : output+=(0*Math.pow(10,exponent));
+				break;
+				}
+				exponent++;
+					
+			}
+			if (output>=2147483647) {
+				System.out.println("Your input has digit that out of Int's range");
+			}
+			else {
 				System.out.println("Your output: "+output);
 			}
+				
+		}
 			
-			else {
-				System.out.println("Your input has no digit.");
-			}
+		else {
+			System.out.println("Your input has no digit.");
 		}
 		
 		
-		catch(Exception e){
-			System.out.println("Your input is out of Int's range.");
-		}
-		
-
 	}
 
 }
