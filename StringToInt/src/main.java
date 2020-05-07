b5e119cb10dc63c9b9dad00d4f0c513b2e91f776
rsmith6321 <rsmith@jinnawats-MacBook-Pro.local>
2020-05-07 12:05:52 +0700

    update algorithm and jar file

--- StringToInt/src/main.java
+++ StringToInt/src/main.java
@@ -1,69 +1,81 @@
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Your input: ");
 		String input = sc.nextLine();
-		String getDigit="";
-		
-			
-		for(int i=0;i<input.length();i++) {
-			if(Character.isDigit(input.charAt(i))){
-				getDigit=getDigit+input.charAt(i);
-					
-			}
-			else continue;
-		}
-			
-		if(getDigit!="") {
+		int exponent = 0;
+		int output=0;
+		Boolean hasDigit=false;
+		Boolean outOfRange=false;
 
-			int output=0;
-			double exponent = 0;
-				
-			for(int i = getDigit.length()-1;i>=0;i--){
-					
-				switch(getDigit.charAt(i)) {
-				case '1' : output+=(1*Math.pow(10,exponent));
+		for(int i = input.length()-1;i>=0;i--) {
+
+			if(Character.isDigit(input.charAt(i))){
+				hasDigit=true;
+
+				switch(input.charAt(i)) {
+				case '1' : output+=1*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '2' : output+=(2*Math.pow(10,exponent));
+				case '2' : output+=2*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '3' : output+=(3*Math.pow(10,exponent));
+				case '3' : output+=3*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '4' : output+=(4*Math.pow(10,exponent));
+				case '4' : output+=4*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '5' : output+=(5*Math.pow(10,exponent));
+				case '5' : output+=5*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '6' : output+=(6*Math.pow(10,exponent));
+				case '6' : output+=6*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '7' : output+=(7*Math.pow(10,exponent));
+				case '7' : output+=7*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '8' : output+=(8*Math.pow(10,exponent));
+				case '8' : output+=8*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '9' : output+=(9*Math.pow(10,exponent));
+				case '9' : output+=9*(Math.pow(10,exponent));
+				exponent++;
 				break;
-				case '0' : output+=(0*Math.pow(10,exponent));
+				case '0' : output+=0*(Math.pow(10,exponent));
+				exponent++;
 				break;
 				}
-				exponent++;
-					
+
+
+				if(exponent>10||output>=Integer.MAX_VALUE) {
+					outOfRange=true;
+				}
+
+
+
+
 			}
-			if (output>=2147483647) {
-				System.out.println("Your input has digit that out of Int's range");
-			}
-			else {
-				System.out.println("Your output: "+output);
-			}
-				
+
+
+
 		}
-			
-		else {
-			System.out.println("Your input has no digit.");
+
+
+		//OUTPUT
+		if(hasDigit==false) {
+			System.out.println("Your input has no digit");
 		}
-		
-		
+		else if(outOfRange==true){
+			System.out.println("Your input has number that out of int's range. ");
+		}
+		else System.out.println("Your output: "+output);
+
+
+
 	}
 
 }
